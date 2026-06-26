70021f4a6a303339f9e249296144ca28ee73679b
ramyanagesh29 <ramyanagesh05@gmail.com>
2026-06-26 17:17:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function registerEvent() {
+    alert("Thank you for registering. Your event registration has been submitted successfully.");
+}
+
+function sendMessage() {
+    alert("Thank you for contacting us. We will get back to you soon.");
+}
+
+function loginUser() {
+    alert("Welcome to Event Management System.");
+}
+
+function addEvent() {
+    alert("New event added successfully.");
+}
+
+function updateEvent() {
+    alert("Event details updated successfully.");
+}
+
+function deleteEvent() {
+    alert("Event deleted successfully.");
+}
